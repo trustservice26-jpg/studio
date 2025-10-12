7daa2911a0b3eebe8a7ddcf512a3a3706cc535ee
Trust Service <trustservice26@gmail.com>
2025-10-12 16:58:53 +0000

    in smart card make the head titlte llittle big with adjust same with the

--- src/components/smart-card/smart-card.tsx
+++ src/components/smart-card/smart-card.tsx
@@ -83,10 +83,10 @@ export function SmartCard({ member, side, isPdf = false, language: propLanguage 
         <div className="p-[8px_16px] border-b border-gray-200 flex items-center gap-3">
           <HeartHandshake className="text-green-800 h-7 w-7 shrink-0" />
           <div className="flex flex-col justify-center">
-              <h1 className="font-card_headline text-[0.95rem] font-bold m-0 leading-tight whitespace-nowrap">
+              <h1 className="font-card_headline text-[1rem] font-bold m-0 leading-tight whitespace-nowrap">
                 <span className="text-green-800">HADIYA</span>{' – '}<span className="text-yellow-600">{` মানবতার উপহার`}</span>
               </h1>
-              <p className="font-subheadline text-[0.4rem] text-gray-600 m-0 font-normal whitespace-nowrap mt-1">
+              <p className="font-subheadline text-[0.45rem] text-gray-600 m-0 font-normal whitespace-nowrap pt-1">
                 {'শহীদ লিয়াকত স্মৃতি সংঘ ( চান্দগাঁও ) -এর অধীনে একটি সম্প্রদায়-চালিত উদ্যোগ'}
               </p>
           </div>
